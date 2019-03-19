a319de2ba3a2e9037decf7d5ac90555d46f7875a
Drew Alexander <dru@DruBookV2.modem>
2019-03-20 01:33:45 +0800

    LEss CRaShy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
 client.on('messageReactionAdd', reacc => {
   
   if (reacc.emoji.name === '💥') {
-    for (let index = 0; index < 19; index++) {
+    for (let index = 0; index < 10; index++) {
       var emote = emojis[Math.floor(Math.random() * emojis.length)];
       console.log("Reacting:" + emote);
       reacc.message.react(emote).catch(error => {
